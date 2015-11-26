56276b24efc6d176a9fe66e3f99ed3b223b197a1
Apinya Kongtragul <it58070163@office365.it.kmitl.ac.th>
2015-11-26 14:33:23 +0700

    Presentation--Wix.com
    Presentation this project on wix.com--
    http://milkkytwinz.wix.com/earthquake-trends

--- Testing.py
+++ Testing.py
@@ -1,6 +1,9 @@
 """Test GitHub working"""
+import pandas as pd
 def testing():
     """Testing"""
+    data = input("Enter Location : ")
+    locations = data[['Longitude','Latitude']].as_matrix()
     print("EarthquakeTrends Project")
     print("Test2")
 
