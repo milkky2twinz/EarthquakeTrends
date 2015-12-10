977ee3f344398052e1885b90caf35737d26c87ae
sweetbellza <it58070126@office365.it.kmitl.ac.th>
2015-12-10 20:18:07 +0700

    Add Doc String for Help
    Add Doc String for Help of Earthquake stat in Myanmar that affect
    Thailand 2004-2014.

--- Earthquake stat in myanmar that affect thailand 2004-2014.py
+++ Earthquake stat in myanmar that affect thailand 2004-2014.py
@@ -1,7 +1,16 @@
+""""
+************************************************************
+*** Earthquake stat in Myanmar that affect Thailand 2004-2014. ****
+***                 Create Graph by module "plotly"                ****
+************************************************************
+"""
 import plotly.plotly as py
 from plotly.graph_objs import *
+
+#Login Plotly by using account username and api_key of plotly for open graph on browser.
 py.sign_in('bbellkungdesu', 'b3ngpu9r4g')
 
+#Pull data from Database for bring create graph.
 data = Data([
     Scatter(
         x=['2004-12-26', '2004-12-30', '2005-09-18', '2006-01-24', '2006-09-27', '2006-09-28', '2006-09-28', '2006-10-08', '2007-05-15', '2007-05-16', '2007-06-23', '2007-11-02', '2008-08-21', '2008-09-22', '2010-03-20', '2010-07-06', '2011-02-04', '2011-03-24', '2011-05-10', '2012-11-11', '2012-11-11', '2012-12-20', '2013-02-07', '2013-04-11', '2013-05-07', '2015-05-24'],
@@ -12,6 +21,8 @@ data = Data([
         uid='7de5d6'
     )
 ])
+
+#define layout and scope of graph.
 layout = Layout(
     annotations=Annotations([
         Annotation(
@@ -62,5 +73,8 @@ layout = Layout(
         type='linear'
     )
 )
+
+#Plot graph.
 fig = Figure(data=data, layout=layout)
+#Open graph on any browser.
 plot_url = py.plot(fig)
